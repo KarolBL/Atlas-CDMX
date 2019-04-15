6e15b9b159a04fedd16df0b9ab6db0361b93571f
Cristóbal Fresno <cfresno@inmegen.gob.mx>
2019-04-15 02:44:25 -0500

    First module commit

---
+++ analysis/005_mortality_kriging/README.md
@@ -0,0 +1,4 @@
+# 005_mortality_kriging
+
+Obtain the kriged data using the fitted variogram model
+
---
+++ analysis/005_mortality_kriging/bin/kriging.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+############################################################
+## Goal: Obtain the kriging estimations
+############################################################
+library("optparse")
+library("parallel")
+library("gstat")
+library("sp")
+library("spacetime")
+library("gtools")
+library("reshape2")
+############################################################
+option_list <- list(
+  make_option(
+    c("-f", "--file"), 
+    type = "character", 
+    default = NULL, 
+    help = "dataset file name", 
+    metavar = "character"
+  ),
+  make_option(
+    c("-o", "--out"), 
+    type = "character", 
+    default = "out.RData", 
+    help = "output RData file name [default= %default]", 
+    metavar = "character"
+  ),
+  make_option(
+    c("-c", "--cores"), 
+    type = "integer", 
+    default = "7", 
+    help = "number of cores to use [default= %default]", 
+    metavar = "integer"
+  )
+) 
+
+#Build the parse object
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+##Check for parsing options
+if(is.null(opt$file)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+############################################################
+# Debuging
+# opt$file <- "../data/mortality_general.RData"
+# opt$out <- "../results/mortality_general.RData"
+# opt$cores <- 3
+options(mc.cores = opt$cores)
+options(width = 150)
+############################################################
+## Read the data
+load(opt$file)
+
+############################################################
+#Variogram models 
+############################################################
+#save(
+#  bestmodel2use, 
+#  file = opt$out,
+#  compress = "xz"
+#)
+############################################################################
+## The end
+############################################################################

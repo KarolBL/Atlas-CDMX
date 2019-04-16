2ee6406f79703c791708b883f27b215155adb2bd
Cristóbal Fresno <cfresno@inmegen.gob.mx>
2019-04-16 14:33:46 -0500

    First Module commit

--- analysis/004_get_centroids/bin/centroids_neighborhood_level.R
+++ analysis/004_get_centroids/bin/centroids_neighborhood_level.R
@@ -12,6 +12,7 @@ library("rgdal")
 library("leaflet")
 library("mapview")
 library("ggplot2")
+# http://mazamascience.com/WorkingWithData/?p=1494
 #############################################
 # Upload polygons at neighborghood level
 #############################################
@@ -33,7 +34,7 @@ pol <- subset(pol, ST_NAME=="DISTRITO FEDERAL")
 #call the function "gCentroidWithin"
 #############################################
 gCentroidWithin <- function(pol) {
-  require(rgeos)
+  require("rgeos")
   
   pol$.tmpID <- 1:length(pol)
   # initially create centroid points with gCentroid
@@ -48,11 +49,13 @@ gCentroidWithin <- function(pol) {
     gIntersects(pol[x,], centsDF[x, ])
   })
   # substitue outside centroids with points INSIDE the polygon
-  newPoints <- SpatialPointsDataFrame(gPointOnSurface(pol[!centsInOwnPoly, ], 
+  if(nrow(pol[!centsInOwnPoly, ])>0){
+    newPoints <- SpatialPointsDataFrame(gPointOnSurface(pol[!centsInOwnPoly, ], 
                                                       byid = T), 
                                       pol@data[!centsInOwnPoly,])
-  newPoints$isCentroid <- FALSE
-  centsDF <- rbind(centsDF[centsInOwnPoly,], newPoints)
+    newPoints$isCentroid <- FALSE
+    centsDF <- rbind(centsDF[centsInOwnPoly,], newPoints)
+  }
   
   # order the points like their polygon counterpart based on `.tmpID`
   centsDF <- centsDF[order(centsDF$.tmpID),]
@@ -76,25 +79,64 @@ pol@data <- cbind(pol@data,
                      coordinates()
 )
 
+#############################################
+pol@data$fill <- 1:nrow(pol@data)
+
+tlalpan <- subset(pol, MUN_NAME == "TLALPAN")
+pol <- tlalpan
+pol <- gUnaryUnion(pol)
+
+coordinates(gCentroid(pol))
+
+plot(pol)
+plot(tlalpan)
+
+SpatialPointsDataFrame
+
+aux <- SpatialPointsDataFrame(coordinates(pol), tlalpan@data[1,,drop=FALSE])
+plot(aux)
+
+class(tlalpan)
+class(pol)
+SpatialPolygons
+SpatialPolygonsDataFrame
+?SpatialPolygonsDataFrame
+aa <- SpatialPolygonsDataFrame(pol, data.frame(test=1, id=1))
+plot(aa)
+gCentroidWithin(aa)
+points(coordinates(aa))
+
+
+#create a data.frame from our spatial object
+pol2 <- fortify(pol, region = "OBJECTID")
+# merge the "fortified" data with the data from our spatial object
+pol2 <- merge(pol2, pol@data, by.y = "OBJECTID", by.x = "id")
+
+#gUnion
+
 # Plot map at neighborghood level
-p <- ggplot(pol) +
+p <- ggplot(
+  data = pol2,
+  aes(
+    x = long,
+    y = lat,
+    group = id#,
+    #fill = MUN_NAME
+  )
+) +
   geom_polygon(
-    data=pol,
-    aes(
-      x = long,
-      y = lat,
-      group = group
-    ), 
-    fill = "#F17521",
-    color = "#1F77B4",
-    alpha = .6
+   alpha = .6
   ) +
-  geom_point(
-    data = pol@data,
-    aes(
-      x = x,
-      y = y
-    )
+  geom_path(color = "white")+
+  #geom_point() + #los del polígono
+  coord_equal() +
+  xlab("Longitude")+
+  ylab("Latitud")+
+  theme_bw()+
+  theme(
+    legend.position = "bottom" 
+    #title = element_blank()
+    #axis.text = element_blank()
   )
 p
 
